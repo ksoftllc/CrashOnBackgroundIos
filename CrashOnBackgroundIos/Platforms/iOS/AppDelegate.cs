65190a802e976140404bd4fc3c8d01fd3f1d2497
Chuck Krutsinger <ckrutsinger@bluedotsolutions.com>
2024-11-07 16:16:45 -0700

    Add project files.

---
+++ CrashOnBackgroundIos/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CrashOnBackgroundIos
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
