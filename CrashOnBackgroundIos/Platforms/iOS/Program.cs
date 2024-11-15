b003eb9d867ed8eb3d03f3816d535b5f0cfe5e2d
chuckkrutsinger <ckrutsinger@bluedotsolutions.com>
2024-11-14 19:07:46 -0700

    reproduce crash

--- CrashOnBackgroundIos/Platforms/iOS/Program.cs
+++ CrashOnBackgroundIos/Platforms/iOS/Program.cs
@@ -1,16 +1,28 @@
-﻿using ObjCRuntime;
+﻿
 using UIKit;
 
 namespace CrashOnBackgroundIos
 {
     public class Program
     {
+
+        public Program()
+        {
+        }
+
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
-            UIApplication.Main(args, null, typeof(AppDelegate));
+            try
+            {
+                UIApplication.Main(args, null, typeof(AppDelegate));
+            }
+            catch (System.Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
